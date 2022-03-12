ef5ed1d61686889e6ecb4bec974d04b1a79e83e8
HVTURINGGA <x@disism.com>
2022-03-12 19:27:33 +0800

    add(feature)[app]: health check status.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sarkv';
+  status = false;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get<Status>('https://halfmemories.com/ping').subscribe((data) => {
+      if (data.message === 'pong!') {
+        this.status = true;
+      }
+    });
+  }
+
+
+}
+
+export interface Status {
+  message: string;
 }
